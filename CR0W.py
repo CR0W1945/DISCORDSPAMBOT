3ef7c84002f936de0afb3cf1f8dc52c38c53af61
CR0W_1945 <115955642+CR0W1945@users.noreply.github.com>
2022-10-17 12:20:31 +0100

    ENJOY

---
+++ CR0W.py
@@ -0,0 +1,21 @@
+import time
+import requests
+import pyfiglet
+
+banner = pyfiglet.figlet_format("CR0W", font = "slant" )
+print(banner)
+spammsg = input("ur message u want send it to victim;-;: ")
+wb = input("plz enter ur webhook url : ")
+def work(spammsg, wb):
+    while True:
+        try:
+            data = requests.post(wb, json={'content': spammsg})
+            if data.status_code == 204:
+                print(f"has been sent:  {spammsg}")
+        except:
+            print("webhooknotwork :" + wb)
+            time.sleep(5)
+            exit()
+sleep = 1
+while sleep == 1:
+    work(spammsg, wb)
